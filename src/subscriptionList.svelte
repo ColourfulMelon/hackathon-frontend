4fb854ba8d6c66e19cc163fd992f195ac3707ba7
Jordyn Nitch <NTCJOR001@myuct.ac.za>
2024-02-07 01:09:09 +0200

    added separate subscriptionList.svelte component

---
+++ src/subscriptionList.svelte
@@ -0,0 +1,56 @@
+<script lang="ts">
+
+    import Subscription from "./subscription.svelte";
+
+    export let title: string;
+    export let description: string;
+    export let cost: string;
+    export let logo: string
+
+    function handleClick() {
+
+    }
+
+
+</script>
+
+<div class="subscriptionList">
+    <Subscription
+            title="Example Title"
+            description="Example Description"
+            cost="50"
+            logo="/public/vite.svg"
+    />
+    <Subscription
+            title="Example Title"
+            description="Example Description"
+            cost="50"
+            logo="/public/vite.svg"
+    />
+    <Subscription
+            title="Example Title"
+            description="Example Description"
+            cost="50"
+            logo="/public/vite.svg"
+    />
+
+    <Subscription
+            title="Example Title"
+            description="Example Description"
+            cost="50"
+            logo="/public/vite.svg"
+    />
+</div>
+
+<style>
+    .subscriptionList {
+        display: grid;
+
+        justify-content: flex-start;
+        align-items: flex-start;
+        gap: 5rem;
+        width: 80vw;
+        /*height: 20vh;*/
+
+    }
+</style>
